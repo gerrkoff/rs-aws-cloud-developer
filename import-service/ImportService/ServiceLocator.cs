d1b3c5f1eb7f31697e9df6f66699fcaeef9ee261
gerrkoff <antonkofa@gmail.com>
2024-06-29 15:09:33 +0200

    task-6

--- import-service/ImportService/ServiceLocator.cs
+++ import-service/ImportService/ServiceLocator.cs
@@ -1,5 +1,6 @@
 using Amazon.Lambda.Core;
 using Amazon.S3;
+using Amazon.SQS;
 using ImportService.Services;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -9,6 +10,8 @@ namespace ImportService;
 public static class ServiceLocator
 {
     public static IEnvProvider EnvProvider { get; } = new EnvProvider();
+
+    public static IAmazonSQS SqsClient { get; } = new AmazonSQSClient();
 
     public static IImportsService ImportsService { get; } = new ImportsService(new AmazonS3Client(), EnvProvider);
 }
--- product-service/ProductService/ServiceLocator.cs
+++ product-service/ProductService/ServiceLocator.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using Amazon.SimpleNotificationService;
 using ProductService.Services;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -8,6 +9,8 @@ namespace ProductService;
 public static class ServiceLocator
 {
     public static IEnvProvider EnvProvider { get; } = new EnvProvider();
+
+    public static IAmazonSimpleNotificationService SnsClient { get; } = new AmazonSimpleNotificationServiceClient();
     
     public static IProductsService ProductsService { get; } = new ProductsService(EnvProvider, new Mapper());
 }
